14bf98ed3b5b1ebaa57b623d483c72f15097f7aa
mikecardona076 <mikecardona076@gmail.com>
2020-06-09 20:02:23 -0700

    #1.4

--- Figuras_geometricas.java
+++ Figuras_geometricas.java
@@ -1,19 +1,42 @@
+import java.util.ArrayList;
 public class Figuras_geometricas {
     public static void main(String[] args) {
-        Rectangulo rec = new Rectangulo(4, 2);
+        // Rectangulo rec = new Rectangulo(0,0);
 
-        Area_imprimir(rec);
-        Perimetro_imprimir(rec);
+        // Area_imprimir(rec);
+        // Perimetro_imprimir(rec);
+
+        ArrayList<Rectangulo> ListaRectangulo = new ArrayList<Rectangulo>();
+        ListaRectangulo.add(new Rectangulo(2, 3));
+        ListaRectangulo.add(new Rectangulo(6, 4));
+        ListaRectangulo.add(new Rectangulo(2, 3));
+        ListaRectangulo.add(new Rectangulo(6, 4));
+        
+        ImprimirAreasVariosRectangulos(ListaRectangulo);
+        ImprimirAreasVariosPerimetros(ListaRectangulo);
+
     }
 
-    public static void Area_imprimir(Rectangulo r) {
-        r.Area_rectangulo();
-        
+    public static void ImprimirAreasVariosRectangulos(ArrayList<Rectangulo> Lista){
+        for(Rectangulo R:Lista){
+            System.out.println("Area: " + R.Area_rectangulo());
+        }
     }
-    public static void Perimetro_imprimir(Rectangulo r) {
-        r.Perimetro();
-        
+
+    public static void ImprimirAreasVariosPerimetros(ArrayList<Rectangulo> Lista){
+        for(Rectangulo R:Lista){
+            System.out.println("Perimetro: " + R.Perimetro_rectangulo());
+        }       
     }
+
+    // public static void Area_imprimir(Rectangulo r) {
+    //     r.Area_rectangulo();
+        
+    // }
+    // public static void Perimetro_imprimir(Rectangulo r) {
+    //     r.Perimetro_rectangulo();
+        
+    // }
 
     
 }
--- Rectangulo.java
+++ Rectangulo.java
@@ -2,18 +2,49 @@ public class Rectangulo {
     private static double base;
     private static double altura;
 
-    public Rectangulo(double d, double e) {
-        this.altura = d;
-        this.base = e;
+    public Rectangulo(double altura, double base) {
+        super();
+        this.altura = altura;
+        this.base = base;
     }
 
-    public void Area_rectangulo() {
-        System.out.println("EL Area: " + base * altura);
+    public double getBase(){
+        return base;
+    }
+
+    public void setBase(double base){
+        this.base = base;
+    }
+
+    public double getAltura(){
+        return altura;
+    }
+
+    public void setAltura(double altura){
+        this.altura = altura;
+    }
+
+    //Funciones para el Area 
+    public double Area_rectangulo() {
+        return Rectangulo.CalcularArea(getBase(),getAltura());
  
     }
-    public void Perimetro(){
-        System.out.println("Perimetro: " + 2 * (base + altura));
+
+    public static double CalcularArea(double base, double altura){
+        return base * altura;
 
     }
+
+    //Funciones para el perimetro
+
+    public double Perimetro_rectangulo(){
+        return Rectangulo.CalcularPerimetro(getBase(),getAltura());
+ 
+    }
+
+    public static double CalcularPerimetro(double base, double altura){
+        return (2 * (base + altura));
+    }
+    
     
 }
